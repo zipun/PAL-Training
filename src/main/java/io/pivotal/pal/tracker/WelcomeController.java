e759550d04043c0b1d91c056f1e5dbdd53109fbd
jayaprakash <jprakash.jetti@gmail.com>
2019-01-07 16:56:58 +0530

    Added controller

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,14 +1,32 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+
+    private final String message;
+
+    public WelcomeController(
+            @Value("${WELCOME_MESSAGE:HELLO}") String message
+    ) {
+        this.message = message;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return message;
+    }
+
+    @GetMapping("/welcome")
+    public String sayHello(String msg) {
+        if (msg != null) {
+            return msg;
+        }
+        return this.message;
     }
 }
 
